f598c51297cd3ed32833cabef42c74b16ff74a9e
commiter <commit@github.com>
2025-03-20 11:05:06 +0800

    Update version to v0.0.10

--- commons/error.go
+++ commons/error.go
@@ -4,5 +4,6 @@ import "errors"
 
 var (
 	ErrInsufficientCredits = errors.New("insufficient credits, please purchase more and try again later")
+	ErrInvalidParam        = errors.New("invalid parameter, please refresh the page and try again later")
 	ErrInternalError       = errors.New("internal application error, please refresh the page and try again later")
 )
