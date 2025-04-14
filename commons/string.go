e25f74615cb9ecb073c0ac61dcc90ecd5f7e4e5e
commiter <commit@github.com>
2025-04-14 09:36:39 +0800

    Update version to v0.0.42

--- commons/string.go
+++ commons/string.go
@@ -2,6 +2,14 @@ package commons
 
 import "strings"
 
+func UpperCaseFirstChar(s string) string {
+	if len(s) == 0 {
+		return s
+	}
+
+	return strings.ToUpper(s[:1]) + s[1:]
+}
+
 func StringSliceContainsNoCase(haystack []string, needle string) bool {
 	for _, row := range haystack {
 		if strings.EqualFold(row, needle) {
