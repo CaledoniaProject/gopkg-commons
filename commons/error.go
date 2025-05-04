7195b6e4010edb03c5b557085c4e0c27b3333974
commiter <commit@github.com>
2025-05-05 06:45:12 +0800

    Update version to v0.0.44

--- commons/error.go
+++ commons/error.go
@@ -5,6 +5,7 @@ import "errors"
 var (
 	ErrAuthFailed          = errors.New("authentication failed")
 	ErrNoKey               = errors.New("key field missing")
+	ErrNoCoupon            = errors.New("no coupon available")
 	ErrInsufficientCredits = errors.New("insufficient credits, please purchase more and try again later")
 	ErrInvalidParam        = errors.New("invalid parameter, please refresh the page and try again later")
 	ErrInternalError       = errors.New("internal application error, please refresh the page and try again later")
