ad20d7b6d10f2de4f20cef2e840ebd05560d6674
commiter <commit@github.com>
2025-05-10 10:10:52 +0800

    Update version to v0.5.1

--- commons/size.go
+++ commons/size.go
@@ -6,12 +6,22 @@ func FormatBytes(bytes float64) string {
 	var (
 		sizes = []string{"B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"}
 		i     int
+		neg   = bytes < 0
 	)
+
+	if neg {
+		bytes = -bytes
+	}
 
 	for bytes >= 1024 && i < len(sizes)-1 {
 		bytes /= 1024
 		i++
 	}
 
-	return fmt.Sprintf("%.2f %s", bytes, sizes[i])
+	sign := ""
+	if neg {
+		sign = "-"
+	}
+
+	return fmt.Sprintf("%s%.2f %s", sign, bytes, sizes[i])
 }
