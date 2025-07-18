447c98c01277b04dfcd2eafced97f91e828a3d73
commiter <commit@github.com>
2025-07-18 17:25:17 +0800

    Update version to v0.5.10

--- commons/nullabletime.go
+++ commons/nullabletime.go
@@ -1,7 +1,9 @@
 package commons
 
 import (
+	"database/sql/driver"
 	"encoding/json"
+	"fmt"
 	"strings"
 	"time"
 )
@@ -21,4 +23,29 @@ func (m *NullableTime) UnmarshalJSON(data []byte) error {
 
 func (m *NullableTime) IsZero() bool {
 	return time.Time(*m).IsZero()
+}
+
+// gorm
+
+func (m NullableTime) Value() (driver.Value, error) {
+	t := time.Time(m)
+	if t.IsZero() {
+		return nil, nil
+	}
+	return t, nil
+}
+
+func (m *NullableTime) Scan(value interface{}) error {
+	if value == nil {
+		*m = NullableTime{}
+		return nil
+	}
+
+	switch v := value.(type) {
+	case time.Time:
+		*m = NullableTime(v)
+		return nil
+	default:
+		return fmt.Errorf("cannot scan type %T into NullableTime", value)
+	}
 }
--- commons/url.go
+++ commons/url.go
@@ -2,6 +2,8 @@ package commons
 
 import (
 	"net/url"
+
+	"github.com/pkg/errors"
 )
 
 func MustParseURL(input string) *url.URL {
@@ -11,4 +13,19 @@ func MustParseURL(input string) *url.URL {
 	}
 
 	return urlObj
+}
+
+func ResolveURL(currentURL string, href string) (string, error) {
+	current, err := url.Parse(currentURL)
+	if err != nil {
+		return "", errors.Wrapf(err, "parse currentURL: %s", currentURL)
+	}
+
+	ref, err := url.Parse(href)
+	if err != nil {
+		return "", errors.Wrapf(err, "parse href: %s", href)
+	}
+
+	finalURL := current.ResolveReference(ref)
+	return finalURL.String(), nil
 }
