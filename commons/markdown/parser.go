089759f8448644779e5a31f9affc41beb7af761b
commiter <commit@github.com>
2025-07-29 21:19:17 +0800

    Update version to v0.5.26

--- commons/markdown/parser.go
+++ commons/markdown/parser.go
@@ -7,19 +7,19 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-func NewMarkdownHeader(md string) (*MarkdownHeader, error) {
+func NewMarkdownHeader(md string) (*MarkdownHeader, string, error) {
 	var (
 		header MarkdownHeader
 	)
 
 	parts := strings.SplitN(md, "---", 3)
 	if len(parts) < 3 {
-		return nil, fmt.Errorf("no valid front matter")
+		return nil, "", fmt.Errorf("no valid front matter")
 	}
 
 	if err := yaml.Unmarshal([]byte(strings.TrimSpace(parts[1])), &header); err != nil {
-		return nil, err
+		return nil, "", err
 	} else {
-		return &header, nil
+		return &header, parts[2], nil
 	}
 }
